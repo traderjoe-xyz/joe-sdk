f989b59f2564dd74147788baa41cbf818a0d9933
cryptofish7 <cryptofish7@protonmail.com>
2021-04-15 00:31:44 -0400

    Import JoePair instead of uniswap version

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,11 +1,11 @@
 import { INIT_CODE_HASH } from '../src/constants'
 
-import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+import { bytecode } from '@joe-defi/core/artifacts/contracts/traderjoe/JoePair.sol/JoePair.json'
 import { keccak256 } from '@ethersproject/solidity'
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
 // and load the JSON.
-const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`${bytecode}`])
 
 describe('constants', () => {
   describe('INIT_CODE_HASH', () => {
