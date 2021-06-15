4ce9c223682fc533d42294eff3192fe73a76531c
cryptofish7 <cryptofish7@protonmail.com>
2021-06-15 11:11:18 -0400

    Update test

--- test/pair.test.ts
+++ test/pair.test.ts
@@ -1,21 +1,20 @@
 import { ChainId, Token, Pair, TokenAmount, WAVAX, Price } from '../src'
 
 describe('Pair', () => {
-
-  const FISH = new Token(ChainId.FUJI, '0xAd1c1a22A0eC39b81034b6c4dC01D5AeC406A4Ad', 18, 'FISH', 'FishToken');
-  const MURLOC = new Token(ChainId.FUJI, '0xD6e29Beeb3C37542dCC5C115e44b232ACF487073', 18, 'MURLOC', 'MurlocToken');
+  const FISH = new Token(ChainId.FUJI, '0xAd1c1a22A0eC39b81034b6c4dC01D5AeC406A4Ad', 18, 'FISH', 'FishToken')
+  const MURLOC = new Token(ChainId.FUJI, '0xD6e29Beeb3C37542dCC5C115e44b232ACF487073', 18, 'MURLOC', 'MurlocToken')
 
   describe('constructor', () => {
     it('cannot be used for tokens on different chains', () => {
-      expect(() => new Pair(new TokenAmount(FISH, '100'), new TokenAmount(WAVAX[ChainId.AVALANCHE], '100'), ChainId.FUJI)).toThrow(
-        'CHAIN_IDS'
-      )
+      expect(
+        () => new Pair(new TokenAmount(FISH, '100'), new TokenAmount(WAVAX[ChainId.AVALANCHE], '100'), ChainId.FUJI)
+      ).toThrow('CHAIN_IDS')
     })
   })
 
   describe('#getAddress', () => {
     it('returns the correct address', () => {
-      expect(Pair.getAddress(FISH, MURLOC, ChainId.FUJI)).toEqual('0x9c1111fed56e51790a74Ea02B054e509826840E7')
+      expect(Pair.getAddress(FISH, MURLOC, ChainId.FUJI)).toEqual('0xb875CeCB08CeDAD83fB8738211bc087D0Ee325C2')
     })
   })
 
@@ -27,8 +26,12 @@ describe('Pair', () => {
   })
   describe('#token1', () => {
     it('always is the token that sorts after', () => {
-      expect(new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).token1).toEqual(MURLOC)
-      expect(new Pair(new TokenAmount(FISH, '100'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).token1).toEqual(MURLOC)
+      expect(new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).token1).toEqual(
+        MURLOC
+      )
+      expect(new Pair(new TokenAmount(FISH, '100'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).token1).toEqual(
+        MURLOC
+      )
     })
   })
   describe('#reserve0', () => {
@@ -88,32 +91,44 @@ describe('Pair', () => {
 
   describe('#reserveOf', () => {
     it('returns reserves of the given token', () => {
-      expect(new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '101'), ChainId.FUJI).reserveOf(MURLOC)).toEqual(
-        new TokenAmount(MURLOC, '100')
-      )
-      expect(new Pair(new TokenAmount(FISH, '101'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).reserveOf(MURLOC)).toEqual(
-        new TokenAmount(MURLOC, '100')
-      )
+      expect(
+        new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '101'), ChainId.FUJI).reserveOf(MURLOC)
+      ).toEqual(new TokenAmount(MURLOC, '100'))
+      expect(
+        new Pair(new TokenAmount(FISH, '101'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).reserveOf(MURLOC)
+      ).toEqual(new TokenAmount(MURLOC, '100'))
     })
 
     it('throws if not in the pair', () => {
       expect(() =>
-        new Pair(new TokenAmount(FISH, '101'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).reserveOf(WAVAX[ChainId.FUJI])
+        new Pair(new TokenAmount(FISH, '101'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).reserveOf(
+          WAVAX[ChainId.FUJI]
+        )
       ).toThrow('TOKEN')
     })
   })
 
   describe('#chainId', () => {
     it('returns the token0 chainId', () => {
-      expect(new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).chainId).toEqual(ChainId.FUJI)
-      expect(new Pair(new TokenAmount(FISH, '100'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).chainId).toEqual(ChainId.FUJI)
+      expect(new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).chainId).toEqual(
+        ChainId.FUJI
+      )
+      expect(new Pair(new TokenAmount(FISH, '100'), new TokenAmount(MURLOC, '100'), ChainId.FUJI).chainId).toEqual(
+        ChainId.FUJI
+      )
     })
   })
   describe('#involvesToken', () => {
-    expect(new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).involvesToken(MURLOC)).toEqual(true)
-    expect(new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).involvesToken(FISH)).toEqual(true)
     expect(
-      new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).involvesToken(WAVAX[ChainId.FUJI])
+      new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).involvesToken(MURLOC)
+    ).toEqual(true)
+    expect(
+      new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).involvesToken(FISH)
+    ).toEqual(true)
+    expect(
+      new Pair(new TokenAmount(MURLOC, '100'), new TokenAmount(FISH, '100'), ChainId.FUJI).involvesToken(
+        WAVAX[ChainId.FUJI]
+      )
     ).toEqual(false)
   })
 })
