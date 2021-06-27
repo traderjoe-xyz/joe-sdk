5e67d6d6400f9e722479bacff374ab93eeae656c
robotmurloc-3599 <robot@notmurloc.com>
2021-06-27 22:22:46 +0800

    subgraph urls

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,5 @@ export * from './errors'
 export * from './entities'
 export * from './router'
 export * from './fetcher'
+
+export * from './subgraphs'
---
+++ src/subgraphs.ts
@@ -0,0 +1,27 @@
+import { ChainId } from './constants'
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const BLOCKS_SUBGRAPH: {[chainId in ChainId]: string } = {
+    [ChainId.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks', 
+    [ChainId.FUJI]: ZERO_ADDRESS,
+    [ChainId.AVALANCHE]: 'https://thegraph.com/explorer/subgraph/dasconnor/avalanche-blocks'
+}
+
+export const EXCHANGE_SUBGRAPH: {[chainId in ChainId]: string } = {
+    [ChainId.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/exchange-rinkeby-ii', 
+    [ChainId.FUJI]: ZERO_ADDRESS,
+    [ChainId.AVALANCHE]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/exchange'
+}
+
+export const MASTERCHEF_SUBGRAPH: {[chainId in ChainId]: string } = {
+    [ChainId.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/masterchefv2-rinkeby', 
+    [ChainId.FUJI]: ZERO_ADDRESS,
+    [ChainId.AVALANCHE]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/masterchefv2'
+}
+
+export const DEXCANDLES_SUBGRAPH: {[chainId in ChainId]: string } = {
+    [ChainId.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/dexcandles-rinkeby-ii', 
+    [ChainId.FUJI]: ZERO_ADDRESS,
+    [ChainId.AVALANCHE]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/dexcandles'
+}
