416583248e3927782957ba5e1bcfddcd65071c25
robotmurloc-3599 <robot@notmurloc.com>
2021-06-30 02:14:12 +0800

    added bar subgraph

--- src/subgraphs.ts
+++ src/subgraphs.ts
@@ -25,3 +25,9 @@ export const DEXCANDLES_SUBGRAPH: {[chainId in ChainId]: string } = {
     [ChainId.FUJI]: ZERO_ADDRESS,
     [ChainId.AVALANCHE]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/dexcandles'
 }
+
+export const BAR_SUBGRAPH: {[chainId in ChainId]: string } = {
+    [ChainId.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/bar-rinkeby-ii', 
+    [ChainId.FUJI]: ZERO_ADDRESS,
+    [ChainId.AVALANCHE]: 'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/bar'
+}
