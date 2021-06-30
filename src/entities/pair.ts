6aa8c4c30c692fd00a3fcca7e8e9f388ebdf30b0
cryptofish7 <cryptofish7@protonmail.com>
2021-06-30 01:16:22 -0400

    Rename to pair to JLP Joe Liquidity

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,8 +55,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token, chainId),
       18,
-      'AXP',
-      'avaXwap'
+      'JLP',
+      'Joe Liquidity'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
