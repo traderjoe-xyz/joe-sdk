366fbf75738ff187ebfd8c5280960221e5047fb1
cryptofish7 <cryptofish7@protonmail.com>
2021-06-29 22:48:28 -0400

    Update JoeRoll rinkeby address

--- src/constants.ts
+++ src/constants.ts
@@ -63,7 +63,7 @@ export const MAKER_ADDRESS: { [chainId in ChainId]: string } = {
 }
 
 export const ROLL_ADDRESS: { [chainId in ChainId]: string } = {
-  [ChainId.RINKEBY]: '0xc3D6E59E496A55d62041eA798eacd7c61fb9dEEb',
+  [ChainId.RINKEBY]: '0xD6344FF98d12adD998cBcfaC16A215396e9bCb8c',
   [ChainId.FUJI]: '0x41d5f5F66e4cEd197Ff273308A1c194E9E249f4F',
   [ChainId.AVALANCHE]: '0xefa94DE7a4656D787667C749f7E1223D71E9FD88' // TODO
 }
