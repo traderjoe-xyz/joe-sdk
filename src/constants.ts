9ea22ece0253e5769cd32c89f9ce9ab8540c4264
analogdev.eth <dejijolaoluwa@gmail.com>
2022-07-24 00:21:18 +0100

    Add use of joelens and maxmillion contracts as comments

--- src/constants.ts
+++ src/constants.ts
@@ -189,6 +189,7 @@ export const UNITROLLER_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.AVALANCHE]: '0xdc13687554205E5b89Ac783db14bb5bba4A1eDaC'
 }
 
+// used to get jToken balances, jToken metadata, account’s borrow limit & claimable rewards
 export const JOELENS_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.FUJI]: '0x3A2C79d45EEdcE68d3993F807336D4b0b41741b0',
   [ChainId.AVALANCHE]: '0x994Ed0698F5145211Fd5DAE458dD7d91c2da6CEC'
@@ -199,6 +200,7 @@ export const JAVAX_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.AVALANCHE]: '0xC22F01ddc8010Ee05574028528614634684EC29e'
 }
 
+// used to repay an account’s borrow amount in the jAvax market
 export const MAXIMILLION_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.FUJI]: '0x7b647D11b2E9354fA570B8613BD361AE51e51c27',
   [ChainId.AVALANCHE]: '0xe5cDdAFd0f7Af3DEAf4bd213bBaee7A5927AB7E7'
